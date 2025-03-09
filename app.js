a1bb9d22235a450c46105e3987a7a56aa0020d7d
Arpita Sheokand <arpita2615@gmail.com>
2025-03-09 17:04:45 +0530

    set up basic structure

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("cyber_phoenix/index.ejs");
+});
+
+app.listen(8080, () => console.log("app is listening to port 8080"));
