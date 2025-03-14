925dc562ce64b0774b00efb32074ca41dd522038
PyGamer <abhinav12c1@gmail.com>
2025-03-14 20:38:49 +0530

    make changes to about page

--- public/js/about.js
+++ public/js/about.js
@@ -1,5 +1,42 @@
 gsap.registerPlugin(ScrollTrigger, Flip, Text);
 
+
+// ----------------- Global animations
+
+let horizontalRevealTexts = gsap.utils.toArray(".horizontal-text-reveal");
+horizontalRevealTexts.forEach((text) => {
+    gsap.from(text, {
+        scrollTrigger: {
+            trigger: text,
+            start: "top 95%",
+            end: "center center",
+            scrub: 0.25
+        },
+        backgroundPositionX: "-100%",
+        x: 30        
+    });
+});
+
+let verticalRevealText = gsap.utils.toArray(".vertical-reveal-text");
+
+verticalRevealText.forEach((line) => {
+    line.innerHTML = `<span style="display: inline-block">${line.innerHTML}</span>`;
+});
+
+verticalRevealText = gsap.utils.toArray(".vertical-reveal-text span");
+
+verticalRevealText.forEach((line) => {
+    gsap.from(line, {
+        scrollTrigger: {
+            trigger: line,
+            start: "top 90%",
+            toggleActions: "play pause resume reset",
+            markers: true
+        },
+        yPercent: 110,
+    });
+});
+
 //  ------------- Introduction Section
 
 let timelineFirst = gsap.timeline({
@@ -14,98 +51,4 @@ let timelineFirst = gsap.timeline({
 });
 
 timelineFirst.from(".club-name", {})
-    .from(".word", {});
-
-
-//  ------------- Description Section
-
-// box animation
-gsap.to(".scrolldown-box", {
-    scrollTrigger: {
-        trigger: ".section-club-desc",
-        start: "top bottom",
-        end: "top top",
-        scrub: 0.1,
-        onUpdate: toggleBoxContent,
-        // markers: true
-    },
-    width: "80vw",
-    height: "60vh",
-});
-
-function toggleBoxContent(obj) {
-    let title = document.querySelector(".section-club-desc h3");
-    let description = document.querySelector(".desc-container");
-    if (obj.progress > 0.1) {
-        title.style.display = "flex";
-        description.style.display = "initial";
-    }
-    else {
-        title.style.display = "none";
-        description.style.display = "none";
-    }
-    let box = document.querySelector(".section-club-desc>div");
-    showDesc(obj);
-    if (obj.progress == 1) {
-        box.style.position = "static";
-    }
-    else {
-        box.style.position = "fixed";
-    }
-}
-function showDesc(obj) {
-    if (obj.progress>0.98) {
-        gsap.to(".club-desc", {
-            transform: "translateY(0)"
-        });
-    }
-    else {
-        gsap.to(".club-desc", {
-            yPercent: 100
-        });
-    }
-}
-
-// title animation
-gsap.from(".section-club-desc .horizontal-text-reveal", {
-        scrollTrigger: {
-            trigger: ".section-club-desc h3",
-            start: "top 65%",
-            end: "top top",
-            scrub: 0.1,
-            // markers: true
-        },
-        opacity:0,
-        backgroundPositionX: "-15.5%",
-    });
-
-
-
-//  ------------- Goal Section
-let heading = gsap.utils.toArray(".goals-heading span")
-heading.forEach((word) => {
-    gsap.from(word, {
-        scrollTrigger: {
-            trigger: word,
-            start: "top bottom-=7.5%",
-            end: "bottom center",
-            scrub: 0.1,
-        },
-        backgroundPositionX: "-20%",
-    })
-})
-
-let goal = gsap.utils.toArray(".goal-item");
-goal.forEach((goal) => {
-    gsap.to(goal, {
-        scrollTrigger: {
-            trigger: "goals-list",
-            pin: true,
-            start: "top 10%",
-            scrub: true,
-            markers: true,
-        },
-        xPercent: -100,
-    });
-});
-
+    .from(".club-tagline span", {});
