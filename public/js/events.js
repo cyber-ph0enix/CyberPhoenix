91f0be37564f752aa78c1ce95c0065bd76d95dcd
PyGamer <abhinav12c1@gmail.com>
2025-03-23 15:01:47 +0530

    minor fixes to all pages

--- public/js/events.js
+++ public/js/events.js
@@ -43,3 +43,29 @@ keywords.forEach((kw) => {
         },
     });
 });
+
+let mm = gsap.matchMedia();
+
+mm.add("(max-width: 900px)", () => {
+    const menu = document.querySelector(".nav-element");
+    const hamburger = document.querySelector(".hamburger");
+
+    const tl2 = new gsap.timeline({ paused: true });
+    tl2.to(menu, {
+        height: "auto",
+        position: "relative",
+        opacity: 1,
+        duration: 0.5,
+        ease: "power2.out",
+    });
+
+    let menuOpen = false;
+    hamburger.addEventListener("click", () => {
+        menuOpen = !menuOpen;
+        if (menuOpen) {
+            tl2.play();
+        } else {
+            tl2.reverse();
+        }
+    });
+});
