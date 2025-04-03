d77a74538c377033fdf3bdb7db78da201d067bb2
PyGamer <abhinav12c1@gmail.com>
2025-04-03 20:30:57 +0530

    fix navbar animation on small devices

--- public/js/about.js
+++ public/js/about.js
@@ -123,27 +123,3 @@ document.querySelectorAll(".member").forEach((member) => {
         });
     });
 });
-
-mm.add("(max-width: 900px)", () => {
-    const menu = document.querySelector(".nav-element");
-    const hamburger = document.querySelector(".hamburger");
-
-    const tl2 = new gsap.timeline({ paused: true });
-    tl2.to(menu, {
-        height: "auto",
-        position: "relative",
-        opacity: 1,
-        duration: 0.5,
-        ease: "power2.out",
-    });
-
-    let menuOpen = false;
-    hamburger.addEventListener("click", () => {
-        menuOpen = !menuOpen;
-        if (menuOpen) {
-            tl2.play();
-        } else {
-            tl2.reverse();
-        }
-    });
-});
