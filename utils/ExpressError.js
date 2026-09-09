9cf6cf0bad7f4a8d727a4c69d09173281dcf63b9
ERROR <72090728+git404x@users.noreply.github.com>
2026-09-09 17:55:49 +0530

    refactor: remove legacy ejs

--- utils/ExpressError.js
+++
@@ -1,9 +0,0 @@
-class ExpressError extends Error {
-    constructor(statusCode, message) {
-        super();
-        this.statusCode = statusCode;
-        this.message = "Something went wrong.";
-    }
-}
-
-module.exports = ExpressError;
--- utils/middleware.js
+++
@@ -1,8 +0,0 @@
-module.exports.isAdmin = (req, res, next) => {
-    if (!req.isAuthenticated()) {
-        req.session.redirectUrl = req.originalUrl;
-        res.redirect("/");
-    } else {
-        next();
-    }
-};
